2e66cb304973bf7ee380ae67d35bb8d270632973
Abhishek Singh <abhigva1999@gmail.com>
2024-11-08 15:05:23 +0000

    Updated recent changes

--- app/components/landingpage.js
+++ app/components/landingpage.js
@@ -53,13 +53,13 @@ function LandingPage({ onStartChat }) {
         borderRadius={2}
       >
         <Typography variant="h2" component="h1" align="center">
-          Welcome to MAAN
+          Welcome to Zatch
         </Typography>
         <Typography variant="h6" component="h2" align="center">
           Your Personal AI Companion
         </Typography>
         <Button variant="contained" onClick={onStartChat}>
-          Start Chat
+          Start ME :)
         </Button>
       </Stack>
     </Box>
